0a9430b65f5e084fcdb5766fe1f2f02181caec54
ioanap17 <ioanap17@gmail.com>
2020-02-06 16:47:02 +0200

    Add files via upload

---
+++ PhotoAppDiscoveryService/src/test/java/com/photoapp/discovery/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.photoapp.discovery;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
